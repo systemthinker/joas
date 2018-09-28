557c41216da210c66ec910ef3fc3d691df9ccb36
systemthinker <robbertoutenaar@gmail.com>
2018-09-28 22:01:54 +0200

    improved algoritm for displaying and hiding and toggling elements in dashboard.blade, also added css to support changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,20 @@
                 <div class="vragenSchema">
                     <h5>Bekijk het gehele proces</h5>
                     <dl>
-                        <dt><span>1.</span><text>Soort woning</text></dt>
-                        <dt><span>2.</span><text>Gezinssituatie</text></dt>
-                        <dt><span>3.</span><text>Ondernemer</text></dt>
-                        <dt><span>4.</span><text>Orientatie en informatie</text></dt>
-                        <dt><span>5.</span><text>Inventarisatie en Analyse</text></dt>
-                        <dt><span>6.</span><text>Advies en Motivatie</text></dt>
-                        <dt><span>7.</span><text>Offerte aanvragen</text></dt>
-                        <dt><span>8.</span><text>Acceptatie en finaal akkoord</text></dt>
-                        <dt><span>9.</span><text>Template</text></dt>
+                        <dt class="procesTab active"><span>1.</span><text>Soort woning</text></dt>
+                        <dt class="procesTab"><span>2.</span><text>Gezinssituatie</text></dt>
+                        <dt class="procesTab"><span>3.</span><text>Ondernemer</text></dt>
+                        <dt class="procesTab"><span>4.</span><text>Orientatie en informatie</text></dt>
+                        <dt class="procesTab"><span>5.</span><text>Inventarisatie en Analyse</text></dt>
+                        <dt class="procesTab"><span>6.</span><text>Advies en Motivatie</text></dt>
+                        <dt class="procesTab"><span>7.</span><text>Offerte aanvragen</text></dt>
+                        <dt class="procesTab"><span>8.</span><text>Acceptatie en finaal akkoord</text></dt>
+                        <dt class="procesTab"><span>9.</span><text>Template</text></dt>
                     </dl>
                 </div>
             </div>
 
-            <div class="col-md-5 offset-2">
+            <div class="col-md-5 offset-2 rightRowDashboard">
                 <div class="informatieSchema">
                     <h5>Informatie over uw situatie</h5>
                 </div>@include('layoutsDashboard.1SoortWoning')</div>
--- resources/views/layoutsDashboard/1SoortWoning.blade.php
+++ resources/views/layoutsDashboard/1SoortWoning.blade.php
@@ -1,81 +1,43 @@
-<div class="row rowVraag">
-    <div class="col-sm optie inactive" id="button1">
-        <h6>Koopt u een eerste woning?</h6>
-    </div>
-    <div class="col-sm optie" id="button2" class="inactive">
-        <h6>Bent u een doorstromer?</h6>
-    </div>
-    <div class="col-sm optie" id="button3" class="inactive">
-        <h6>Wilt u oversluiten?</h6>
-    </div>
-</div>
-<div class="resultVraag" id="result"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4 btn btn-lg buttonOrange hidden left border-0" id="pointerLeft"  >terug</div>
-            <div type="button" class="col-sm-4 offset-4 btn btn-lg buttonOrange hidden right border-0 " id="pointerRight" >verder</div>
+
+    <div class="row rowVraag">
+        <div class="col-sm optie" id="button1">
+            <h6>Koopt u een eerste woning?</h6>
+        </div>
+        <div class="col-sm optie" id="button2">
+            <h6>Bent u een doorstromer?</h6>
+        </div>
+        <div class="col-sm optie" id="button3">
+            <h6>Wilt u oversluiten?</h6>
         </div>
     </div>
+    <div class="resultVraag" id="result"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 btn btn-lg buttonOrange hidden left border-0"  >terug</div>
+                <div class="col-sm-4 offset-4 btn btn-lg buttonOrange hidden right border-0 "  >verder</div>
+            </div>
+        </div>
 
 <script type="text/javascript">
-    var btn1 = document.getElementById('button1');
-    var btn2 = document.getElementById('button2');
-    var btn3 = document.getElementById('button3');
+    $('dt.procesTab').click(function(){
+        $('dt.procesTab').removeClass('active');
+        $(this).addClass('active');
+    });
+
+
+    $('.optie').click(function(){
+        $('.optie').removeClass('active');
+        $(this).addClass('active');
+        $('.buttonOrange').removeClass('hidden');
+        var clickedBtnID = $(this).index();
+          result.innerHTML = text[clickedBtnID];
+    });
+
     var result = document.getElementById('result');
-    var button = document.getElementsByClassName("buttonOrange");
-
-    btn1.onclick = function(){
-        var text = "<h5>Aflosvormen starter</h5>" + "<p>Uitleg Aflosvormen starter.<span onmouseover=\"myFunction()\"><i style=\"color: #c1a801\" class=\"fas fa-lightbulb\"></i></span></p> ";
-       result.innerHTML = text;
-            if (btn1.style.backgroundColor !== "rgb(65, 160, 70)") {
-                btn1.style.backgroundColor = "rgb(65, 160, 70)";
-                btn2.style.backgroundColor = "unset";
-                btn3.style.backgroundColor = "unset";
-            }
-        [].forEach.call(button, function (el) {
-
-                el.classList.remove('hidden');
-
-        });
-
-        }
-
-
-
-    btn2.onclick = function(){
-        var text = "<h5>Aflosvormen Doorstromer</h5>" + "<p>Uitleg Aflosvormen Doorstromer.<span onmouseover=\"myFunction()\"><i style=\"color: #c1a801\" class=\"fas fa-lightbulb\"></i></span></p> ";
-
-        result.innerHTML = text;
-
-        if (btn2.style.backgroundColor !== "rgb(65, 160, 70)") {
-            btn2.style.backgroundColor = "rgb(65, 160, 70)";
-            btn1.style.backgroundColor = "unset";
-            btn3.style.backgroundColor = "unset";
-        }
-
-        [].forEach.call(button, function (el) {
-
-            el.classList.remove('hidden');
-
-        });
-    }
-    btn3.onclick = function(){
-        var text = "<h5>Aflosvormen Oversluiter</h5>" + "<p>Uitleg Aflosvormen Oversluiter.<span onmouseover=\"myFunction()\"><i style=\"color: #c1a801\" class=\"fas fa-lightbulb\"></i></span></p> ";
-
-        result.innerHTML = text;
-
-        if (btn3.style.backgroundColor !== "rgb(65, 160, 70)") {
-            btn3.style.backgroundColor = "rgb(65, 160, 70)";
-            btn1.style.backgroundColor = "unset";
-            btn2.style.backgroundColor = "unset";
-        }
-
-        [].forEach.call(button, function (el) {
-
-            el.classList.remove('hidden');
-
-        });
-    }
+    var string1 = "<h5>Aflosvormen starter</h5>" + "<p>Uitleg Aflosvormen starter.<span onmouseover=\"myFunction()\"><i style=\"color: #c1a801\" class=\"fas fa-lightbulb\"></i></span></p>";
+    var string2 = "<h5>Aflosvormen Doorstromer</h5>" + "<p>Uitleg Aflosvormen Doorstromer.<span onmouseover=\"myFunction()\"><i style=\"color: #c1a801\" class=\"fas fa-lightbulb\"></i></span></p> ";
+    var string3 = "<h5>Aflosvormen Oversluiter</h5>" + "<p>Uitleg Aflosvormen Oversluiter.<span onmouseover=\"myFunction()\"><i style=\"color: #c1a801\" class=\"fas fa-lightbulb\"></i></span></p> ";
+    var text = [string1,string2,string3];
 
 
 </script>
